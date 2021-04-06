3a497d5da098244321a232c87bcf497c5d3c82e4
hakan <hakan@NL579>
2021-04-06 18:13:13 +0200

    Removed unused code

--- src/main/java/se/blinfo/regexp/SourceProvider.java
+++ src/main/java/se/blinfo/regexp/SourceProvider.java
@@ -5,9 +5,7 @@ import com.google.gson.GsonBuilder;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  *
@@ -16,7 +14,6 @@ import java.util.Map;
 public class SourceProvider {
 
     private final Gson gson;
-    private final Map<String, RegExp> regExpMap = new HashMap<>();
 
     public SourceProvider() {
         this.gson = new GsonBuilder().create();
