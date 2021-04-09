ca791a4f7c7567294638314f3727edb942208dd1
hakan <hakan@NL579>
2021-04-09 16:58:22 +0200

    Javadoc

---
+++ src/main/java/se/blinfo/regexp/exercise/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * This package contains the exercises for the Regular Expression project.
+ * <p>
+ * The suggested order for the exercises is:
+ * <ol>
+ * <li>UsernameValidator</li>
+ * <li>BankGiroValidator &amp; PlusGiroValidator</li>
+ * <li>VatNumberValidator</li>
+ * <li>UUIDValidator</li>
+ * <li>HtmlLinkParser <sup>*</sup></li>
+ * <li>VersionNumberValidator <sup>*</sup></li>
+ * <li>PasswordValidator</li>
+ * <li>PhoneNumberValidator <sup>*</sup></li>
+ * <li>NameValidator <sup>*</sup></li>
+ * </ol>
+ * <p>
+ * <sup>*</sup> These exercises require some JAVA coding as well.
+ * <p>
+ * To check if your solution is working, see the corresponding Check-class in
+ * the test package, i. e. se.blinfo.regexp.exercise.NameValidatorCheck
+ */
+package se.blinfo.regexp.exercise;
